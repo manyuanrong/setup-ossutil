c2d26c1dffce27bbe6422a46169b8b3c57ebb536
Enok <416828041@qq.com>
2019-10-10 18:04:43 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # setup-ossutil
-配置ossutil命令行工具
+
+配置 ossutil 命令行工具
+
+# Usage
+
+See [action.yml](action.yml)
+
+用例:
+
+```yaml
+steps:
+  - uses: actions/checkout@master
+  - uses: manyuanrong/setup-ossutil@v1
+    with:
+      endpoint: "oss.aliyuncs.com"
+      access-key-id: "your_key_id"
+      access-key-secret: "your_key_secret"
+      sts-token: "sts_token"
+  - run: ossutil cp -rf test.txt oss://bucket/path
+```
+
+参数
+
+- **endpoint**：填写 Bucket 所在地域的域名信息，可参考访问域名和数据中心。
+- **access-key-id**：查看方式请参考创建 AccessKey。
+- **access-key-secret**：查看方式请参考创建 AccessKey。
+- **sts-token**：非必配项，若采用 STS 临时授权方式访问 OSS 需要配置该项，否则置空即可。stsToken 生成方式参考临时访问凭证。
+
+[详细用法参考，阿里云 ossutil 帮助文档](https://help.aliyun.com/document_detail/50452.html)
+
+# License
+
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
---
+++ index.js
@@ -0,0 +1,51 @@
+const core = require("@actions/core");
+const { exec } = require("@actions/exec");
+const toolCache = require("@actions/tool-cache");
+const path = require("path");
+const fs = require("fs");
+
+const url = "http://gosspublic.alicdn.com/ossutil/1.6.7/ossutil64";
+
+async function main() {
+  const ENDPOINT = core.getInput("endpoint");
+  const ACCESS_KEY_ID = core.getInput("access-key-id");
+  const ACCESS_KEY_SECRET = core.getInput("access-key-secret");
+  const STS_TOKEN = core.getInput("sts-token");
+
+  let toolPath = toolCache.find("ossutil", "1.6.7");
+
+  if (!toolPath) {
+    core.info(`downloading from ${url}`);
+    toolPath = await toolCache.downloadTool(url);
+    core.info(`downloaded to ${toolPath}`);
+  }
+
+  const bin = path.join(__dirname, ".bin");
+  if (!fs.existsSync(bin)) {
+    fs.mkdirSync(bin, {
+      recursive: true
+    });
+  }
+
+  fs.copyFileSync(toolPath, path.join(bin, "ossutil"));
+  fs.chmodSync(path.join(bin, "ossutil"), 0o755);
+
+  core.addPath(bin);
+
+  await exec("ossutil", [
+    "config",
+    "-e",
+    ENDPOINT,
+    "-i",
+    ACCESS_KEY_ID,
+    "-k",
+    ACCESS_KEY_SECRET,
+    ...(STS_TOKEN ? ["-t", STS_TOKEN] : []),
+    "-L",
+    "CH"
+  ]);
+}
+
+main().catch(error => {
+  core.setFailed(error.message);
+});
